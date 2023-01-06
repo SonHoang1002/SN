c7bb411b84257ede7cf0b69c5a53723713fc1595
luuthanhhoang <hoangthanhluu1998@gmail.com>
2023-01-06 11:37:54 +0700

    Merge branch 'develop' of https://github.com/EMSO-group/Social-Network-App into develop

--- lib/screen/Page/page_liked.dart
+++ lib/screen/Page/page_liked.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 
-class PageLiked extends StatefulWidget {
+class PageLiked extends StatelessWidget {
   const PageLiked({super.key});
 
-  @override
-  State<PageLiked> createState() => _PageLikedState();
-}
-
-class _PageLikedState extends State<PageLiked> {
   @override
   Widget build(BuildContext context) {
     return const Text('like');
