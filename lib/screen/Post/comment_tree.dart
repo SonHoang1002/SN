78bd9a143f3ed9467ef2654696a397009b9dca6c
luuthanhhoang <hoangthanhluu1998@gmail.com>
2023-01-10 00:14:59 +0700

    handle comment

---
+++ lib/screen/Post/comment_tree.dart
@@ -0,0 +1,254 @@
+import 'package:comment_tree/comment_tree.dart';
+import 'package:flutter/material.dart';
+import 'package:get_time_ago/get_time_ago.dart';
+import 'package:social_network_app_mobile/constant/common.dart';
+import 'package:social_network_app_mobile/screen/Post/PostCenter/post_card.dart';
+import 'package:social_network_app_mobile/theme/colors.dart';
+import 'package:social_network_app_mobile/widget/FeedVideo/feed_video.dart';
+import 'package:social_network_app_mobile/widget/FeedVideo/flick_multiple_manager.dart';
+import 'package:social_network_app_mobile/widget/avatar_social.dart';
+import 'package:social_network_app_mobile/widget/image_cache.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+class CommentTree extends StatefulWidget {
+  const CommentTree({Key? key, this.commentParent}) : super(key: key);
+  final dynamic commentParent;
+
+  @override
+  State<CommentTree> createState() => _CommentTreeState();
+}
+
+class _CommentTreeState extends State<CommentTree> {
+  @override
+  void initState() {
+    GetTimeAgo.setDefaultLocale('vi');
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    dynamic avatarMedia = widget.commentParent['account']['avatar_media'];
+    int replyCount = widget.commentParent['replies_total'];
+    List<Comment> commentChild = replyCount > 0
+        ? [Comment(avatar: 'icon', userName: 'null', content: '')]
+        : [];
+
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 6.0, horizontal: 8.0),
+      child: CommentTreeWidget<Comment, Comment>(
+        Comment(
+          avatar: avatarMedia['preview_url'] ?? linkAvatarDefault,
+          userName: widget.commentParent['account']['display_name'],
+          content: widget.commentParent['content'],
+        ),
+        commentChild,
+        treeThemeData: TreeThemeData(
+            lineColor: replyCount == 0 ? Colors.transparent : greyColor,
+            lineWidth: 0.2),
+        avatarRoot: (context, data) => PreferredSize(
+            preferredSize: const Size.fromRadius(18),
+            child: AvatarSocial(width: 36, height: 36, path: data.avatar!)),
+        avatarChild: (context, data) => PreferredSize(
+          preferredSize: const Size.fromRadius(12),
+          child: data.avatar == 'icon'
+              ? Container(
+                  margin: const EdgeInsets.only(top: 5.0),
+                  child: const Icon(
+                    Icons.reply,
+                    color: greyColor,
+                    size: 14,
+                  ),
+                )
+              : AvatarSocial(width: 30, height: 30, path: data.avatar!),
+        ),
+        contentChild: (context, data) {
+          return replyCount > 0
+              ? Container(
+                  margin: const EdgeInsets.only(top: 5.0),
+                  child: Text(
+                    "$replyCount phản hồi",
+                    style: const TextStyle(
+                        color: greyColor,
+                        fontSize: 11,
+                        fontWeight: FontWeight.w500),
+                  ),
+                )
+              : BoxComment(widget: widget, data: data);
+        },
+        contentRoot: (context, data) {
+          return BoxComment(
+            widget: widget,
+            data: data,
+            post: widget.commentParent,
+          );
+        },
+      ),
+    );
+  }
+}
+
+class BoxComment extends StatelessWidget {
+  final dynamic post;
+  const BoxComment({
+    super.key,
+    required this.widget,
+    required this.data,
+    this.post,
+  });
+
+  final CommentTree widget;
+  final dynamic data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        data.content != ''
+            ? Container(
+                padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.background,
+                    borderRadius: BorderRadius.circular(15)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '${data.userName}',
+                      style: const TextStyle(
+                          fontSize: 12, fontWeight: FontWeight.w500),
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      '${data.content}',
+                      style: const TextStyle(fontSize: 12),
+                    ),
+                  ],
+                ),
+              )
+            : Container(
+                padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 8),
+                child: Text(
+                  '${data.userName}',
+                  style: const TextStyle(
+                      fontSize: 12, fontWeight: FontWeight.w500),
+                )),
+        PostMediaComment(post: post),
+        DefaultTextStyle(
+          style: const TextStyle(
+              color: greyColor, fontSize: 11, fontWeight: FontWeight.w500),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 4),
+            child: Row(
+              children: [
+                const SizedBox(
+                  width: 8,
+                ),
+                const Text('Thích'),
+                const SizedBox(
+                  width: 16,
+                ),
+                const Text('Trả lời'),
+                const SizedBox(
+                  width: 16,
+                ),
+                Text(
+                  GetTimeAgo.parse(
+                      DateTime.parse(widget.commentParent['created_at'])),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class PostMediaComment extends StatefulWidget {
+  final dynamic post;
+  const PostMediaComment({
+    super.key,
+    this.post,
+  });
+
+  @override
+  State<PostMediaComment> createState() => _PostMediaCommentState();
+}
+
+class _PostMediaCommentState extends State<PostMediaComment> {
+  late FlickMultiManager flickMultiManager;
+
+  @override
+  void initState() {
+    super.initState();
+    flickMultiManager = FlickMultiManager();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    dynamic card = widget.post['card'];
+    List medias = widget.post['media_attachments'] ?? [];
+    final size = MediaQuery.of(context).size;
+    renderCard() {
+      if (card['description'] == 'sticky') {
+        return ImageCacheRender(
+          path: card['link'],
+          width: 90.0,
+        );
+      } else if (card['provider_name'] == 'GIPHY') {
+        return Container(
+          margin: const EdgeInsets.only(top: 5.0),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: ImageCacheRender(
+              path: card['link'],
+            ),
+          ),
+        );
+      } else {
+        return PostCard(post: widget.post);
+      }
+    }
+
+    checkIsImage(media) {
+      return media['type'] == 'image' ? true : false;
+    }
+
+    renderMedia() {
+      if (checkIsImage(medias[0])) {
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(12),
+          child: FadeInImage.memoryNetwork(
+            placeholder: kTransparentImage,
+            image: medias[0]['url'],
+            imageErrorBuilder: (context, error, stackTrace) => const SizedBox(),
+          ),
+        );
+      } else {
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(12),
+          child: FeedVideo(
+              path: medias[0]['remote_url'] ?? medias[0]['url'],
+              flickMultiManager: flickMultiManager,
+              image:
+                  medias[0]['preview_remote_url'] ?? medias[0]['preview_url']),
+        );
+      }
+    }
+
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: card != null
+          ? renderCard()
+          : medias.isNotEmpty
+              ? Container(
+                  constraints: BoxConstraints(
+                      maxHeight: size.width * 0.7, maxWidth: size.width * 0.7),
+                  child: renderMedia())
+              : const SizedBox(),
+    );
+  }
+}
--- lib/screen/Post/post_detail.dart
+++ lib/screen/Post/post_detail.dart
@@ -1,13 +1,15 @@
-import 'package:comment_tree/comment_tree.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:social_network_app_mobile/constant/post_type.dart';
+import 'package:social_network_app_mobile/data/post.dart';
 import 'package:social_network_app_mobile/screen/Post/PostCenter/post_center.dart';
 import 'package:social_network_app_mobile/screen/Post/PostFooter/post_footer.dart';
 import 'package:social_network_app_mobile/screen/Post/post_header.dart';
 import 'package:social_network_app_mobile/theme/colors.dart';
 import 'package:social_network_app_mobile/widget/back_icon_appbar.dart';
 import 'package:social_network_app_mobile/widget/text_form_field_custom.dart';
+
+import 'comment_tree.dart';
 
 class PostDetail extends StatelessWidget {
   final dynamic post;
@@ -22,6 +24,7 @@ class PostDetail extends StatelessWidget {
         elevation: 0,
         automaticallyImplyLeading: false,
         title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             const BackIconAppbar(),
             PostHeader(
@@ -55,187 +58,11 @@ class PostDetail extends StatelessWidget {
                         decoration:
                             BoxDecoration(color: Colors.grey.withOpacity(0.3)),
                       ),
-                      Container(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 12, horizontal: 8.0),
-                        child: CommentTreeWidget<Comment, Comment>(
-                          Comment(
-                              avatar: 'null',
-                              userName: 'null',
-                              content:
-                                  'felangel made felangel/cubit_and_beyond public '),
-                          [
-                            Comment(
-                                avatar: 'null',
-                                userName: 'null',
-                                content:
-                                    'A Dart template generator which helps teams'),
-                            Comment(
-                                avatar: 'null',
-                                userName: 'null',
-                                content:
-                                    'A Dart template generator which helps teams generator which helps teams generator which helps teams'),
-                            Comment(
-                                avatar: 'null',
-                                userName: 'null',
-                                content:
-                                    'A Dart template generator which helps teams'),
-                            Comment(
-                                avatar: 'null',
-                                userName: 'null',
-                                content:
-                                    'A Dart template generator which helps teams generator which helps teams '),
-                          ],
-                          treeThemeData: const TreeThemeData(
-                              lineColor: greyColor, lineWidth: 0.2),
-                          avatarRoot: (context, data) => const PreferredSize(
-                            preferredSize: Size.fromRadius(18),
-                            child: CircleAvatar(
-                              radius: 18,
-                              backgroundColor: Colors.grey,
-                              backgroundImage:
-                                  AssetImage('assets/post_background.png'),
-                            ),
-                          ),
-                          avatarChild: (context, data) => const PreferredSize(
-                            preferredSize: Size.fromRadius(12),
-                            child: CircleAvatar(
-                              radius: 12,
-                              backgroundColor: Colors.grey,
-                              backgroundImage:
-                                  AssetImage('assets/post_background.png'),
-                            ),
-                          ),
-                          contentChild: (context, data) {
-                            return Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Container(
-                                  padding: const EdgeInsets.symmetric(
-                                      vertical: 8, horizontal: 8),
-                                  decoration: BoxDecoration(
-                                      color: Colors.grey[100],
-                                      borderRadius: BorderRadius.circular(12)),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        'dangngocduc',
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .caption
-                                            ?.copyWith(
-                                                fontWeight: FontWeight.w600,
-                                                color: Colors.black),
-                                      ),
-                                      const SizedBox(
-                                        height: 4,
-                                      ),
-                                      Text(
-                                        '${data.content}',
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .caption
-                                            ?.copyWith(
-                                                fontWeight: FontWeight.w300,
-                                                color: Colors.black),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                DefaultTextStyle(
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .caption!
-                                      .copyWith(
-                                          color: Colors.grey[700],
-                                          fontWeight: FontWeight.bold),
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(top: 4),
-                                    child: Row(
-                                      children: const [
-                                        SizedBox(
-                                          width: 8,
-                                        ),
-                                        Text('Thích'),
-                                        SizedBox(
-                                          width: 24,
-                                        ),
-                                        Text('Trả lời'),
-                                      ],
-                                    ),
-                                  ),
-                                )
-                              ],
-                            );
-                          },
-                          contentRoot: (context, data) {
-                            return Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Container(
-                                  padding: const EdgeInsets.symmetric(
-                                      vertical: 8, horizontal: 8),
-                                  decoration: BoxDecoration(
-                                      color: Colors.grey[100],
-                                      borderRadius: BorderRadius.circular(12)),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        'dangngocduc',
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .caption!
-                                            .copyWith(
-                                                fontWeight: FontWeight.w600,
-                                                color: Colors.black),
-                                      ),
-                                      const SizedBox(
-                                        height: 4,
-                                      ),
-                                      Text(
-                                        '${data.content}',
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .caption!
-                                            .copyWith(
-                                                fontWeight: FontWeight.w300,
-                                                color: Colors.black),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                DefaultTextStyle(
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .caption!
-                                      .copyWith(
-                                          color: Colors.grey[700],
-                                          fontWeight: FontWeight.bold),
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(top: 4),
-                                    child: Row(
-                                      children: const [
-                                        SizedBox(
-                                          width: 8,
-                                        ),
-                                        Text('Like'),
-                                        SizedBox(
-                                          width: 24,
-                                        ),
-                                        Text('Reply'),
-                                      ],
-                                    ),
-                                  ),
-                                )
-                              ],
-                            );
-                          },
-                        ),
-                      ),
+                      ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: postComment.length,
+                          itemBuilder: ((context, index) =>
+                              CommentTree(commentParent: postComment[index])))
                     ],
                   ),
                 ),
