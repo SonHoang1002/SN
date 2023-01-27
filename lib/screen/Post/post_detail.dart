900531d6d6b3d548042149c1806a7730240e9f8b
luuthanhhoang <hoangthanhluu1998@gmail.com>
2023-01-27 09:44:59 +0700

    chore(post_detail): delete code not use

--- lib/screen/Post/post_detail.dart
+++ lib/screen/Post/post_detail.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:social_network_app_mobile/constant/post_type.dart';
 import 'package:social_network_app_mobile/data/post.dart';
 import 'package:social_network_app_mobile/screen/Post/PostCenter/post_center.dart';
