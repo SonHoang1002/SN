6ae237cf28445c4ed4a3823f5db1a6f852c55704
luuthanhhoang <hoangthanhluu1998@gmail.com>
2023-02-24 10:03:20 +0700

    chore(post_content): handle expanded content

--- lib/screen/Post/PostCenter/post_content.dart
+++ lib/screen/Post/PostCenter/post_content.dart
@@ -2,16 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:social_network_app_mobile/widget/linkify.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class PostContent extends StatelessWidget {
+class PostContent extends StatefulWidget {
   final dynamic post;
   const PostContent({Key? key, this.post}) : super(key: key);
+
+  @override
+  State<PostContent> createState() => _PostContentState();
+}
+
+class _PostContentState extends State<PostContent> {
+  bool isMore = false;
 
   @override
   Widget build(BuildContext context) {
     renderPostContent() {
       final size = MediaQuery.of(context).size;
-      if (post['status_background'] != null) {
-        var backgroundObject = post['status_background'];
+      if (widget.post['status_background'] != null) {
+        var backgroundObject = widget.post['status_background'];
         return Container(
           width: size.width,
           constraints: const BoxConstraints(minHeight: 320),
@@ -25,7 +32,7 @@ class PostContent extends StatelessWidget {
             padding: const EdgeInsets.all(15.0),
             child: Center(
               child: Text(
-                post['content'],
+                widget.post['content'],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                     fontSize: double.parse(backgroundObject['style']['fontSize']
@@ -42,18 +49,40 @@ class PostContent extends StatelessWidget {
       } else {
         return Padding(
             padding: const EdgeInsets.symmetric(horizontal: 12),
-            child: Linkify(
-              text: post['content'] ?? '',
-              onOpen: (link) async {
-                if (await canLaunchUrl(Uri.parse(link.url))) {
-                  await launchUrl(Uri.parse(link.url));
-                } else {
-                  return;
-                }
-              },
-              style: const TextStyle(
-                  fontSize: 15, overflow: TextOverflow.ellipsis),
-              options: const LinkifyOptions(humanize: false),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ClipRect(
+                  child: Align(
+                      alignment: Alignment.topCenter,
+                      heightFactor: isMore ? 1 : 0.3,
+                      child: Linkify(
+                        text: widget.post['content'] ?? '',
+                        onOpen: (link) async {
+                          if (await canLaunchUrl(Uri.parse(link.url))) {
+                            await launchUrl(Uri.parse(link.url));
+                          } else {
+                            return;
+                          }
+                        },
+                        style: const TextStyle(
+                            fontSize: 15, overflow: TextOverflow.ellipsis),
+                        options: const LinkifyOptions(humanize: false),
+                      )),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      isMore = !isMore;
+                    });
+                  },
+                  child: Text(
+                    isMore ? "Thu gọn" : "Xem thêm",
+                    style: const TextStyle(
+                        fontSize: 13, fontWeight: FontWeight.w500),
+                  ),
+                )
+              ],
             )
             // Text(
             //   post['content'],
