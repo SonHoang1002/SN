c99ba442a907bb578f7e43929d2ac942c97ac864
luuhoangthanh <hoangthanhluu1998@gmail.com>
2023-04-26 22:20:56 +0700

    update up moment

--- lib/screen/Moment/moment.dart
+++ lib/screen/Moment/moment.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:social_network_app_mobile/data/background_post.dart';
 import 'package:social_network_app_mobile/providers/moment_provider.dart';
 import 'package:social_network_app_mobile/screen/Moment/moment_pageview.dart';
 import 'package:social_network_app_mobile/theme/colors.dart';
@@ -57,120 +56,121 @@ class _MomentState extends ConsumerState<Moment>
     final size = MediaQuery.of(context).size;
 
     return Scaffold(
+        backgroundColor: Colors.black,
         body: Stack(children: <Widget>[
-      TabBarView(controller: _tabController, children: [
-        momentFollow.isNotEmpty
-            ? MomentPageview(
-                type: 'follow',
-                momentRender: momentFollow,
-                handlePageChange: (value) {
-                  if (value == momentFollow.length - 5) {
-                    ref
-                        .read(momentControllerProvider.notifier)
-                        .getListMomentFollow({
-                      "limit": 10,
-                      "max_id": momentFollow.last['score']
-                    });
-                  }
-                },
-              )
-            : Container(
-                color: Colors.black,
-                width: size.width,
-                height: size.height,
-              ),
-        momentSuggests.isNotEmpty
-            ? MomentPageview(
-                type: 'suggest',
-                momentRender: momentSuggests,
-                handlePageChange: (value) {
-                  if (value == momentSuggests.length - 5) {
-                    ref
-                        .read(momentControllerProvider.notifier)
-                        .getListMomentSuggest({
-                      "limit": 10,
-                      "max_id": momentSuggests.last['score']
-                    });
-                  }
-                },
-              )
-            : Container(
-                color: Colors.black,
-                width: size.width,
-                height: size.height,
-              )
-      ]),
-      Positioned(
-          //Place it at the top, and not use the entire screen
-          top: 0.0,
-          left: 0.0,
-          right: 0.0,
-          child: AppBar(
-            automaticallyImplyLeading: false,
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      widget.isBack != null
-                          ? const BackButton()
-                          : InkWell(
-                              onTap: () => key.currentState!.openDrawer(),
-                              child: const Padding(
-                                padding: EdgeInsets.only(top: 0),
+          TabBarView(controller: _tabController, children: [
+            momentFollow.isNotEmpty
+                ? MomentPageview(
+                    type: 'follow',
+                    momentRender: momentFollow,
+                    handlePageChange: (value) {
+                      if (value == momentFollow.length - 5) {
+                        ref
+                            .read(momentControllerProvider.notifier)
+                            .getListMomentFollow({
+                          "limit": 10,
+                          "max_id": momentFollow.last['score']
+                        });
+                      }
+                    },
+                  )
+                : Container(
+                    color: Colors.black,
+                    width: size.width,
+                    height: size.height,
+                  ),
+            momentSuggests.isNotEmpty
+                ? MomentPageview(
+                    type: 'suggest',
+                    momentRender: momentSuggests,
+                    handlePageChange: (value) {
+                      if (value == momentSuggests.length - 5) {
+                        ref
+                            .read(momentControllerProvider.notifier)
+                            .getListMomentSuggest({
+                          "limit": 10,
+                          "max_id": momentSuggests.last['score']
+                        });
+                      }
+                    },
+                  )
+                : Container(
+                    color: Colors.black,
+                    width: size.width,
+                    height: size.height,
+                  )
+          ]),
+          Positioned(
+              //Place it at the top, and not use the entire screen
+              top: 0.0,
+              left: 0.0,
+              right: 0.0,
+              child: AppBar(
+                automaticallyImplyLeading: false,
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          widget.isBack != null
+                              ? const BackButton()
+                              : InkWell(
+                                  onTap: () => key.currentState!.openDrawer(),
+                                  child: const Padding(
+                                    padding: EdgeInsets.only(top: 0),
+                                    child: Icon(
+                                      Icons.menu,
+                                      color: white,
+                                    ),
+                                  ),
+                                ),
+                          const SizedBox(
+                            width: 7,
+                          ),
+                        ]),
+                    TabBar(
+                        isScrollable: true,
+                        controller: _tabController,
+                        onTap: (index) {},
+                        indicatorColor: Colors.white,
+                        labelColor: Colors.white,
+                        unselectedLabelColor: Colors.white,
+                        indicatorSize: TabBarIndicatorSize.label,
+                        indicatorWeight: 1,
+                        tabs: const [
+                          Tab(
+                            text: "Đang theo dõi",
+                          ),
+                          Tab(
+                            text: "Dành cho bạn",
+                          )
+                        ]),
+                    Row(
+                      children: List.generate(
+                          iconAction.length,
+                          (index) => Container(
+                                width: 30,
+                                height: 30,
+                                margin: const EdgeInsets.only(left: 5),
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: Colors.grey.withOpacity(0.3)),
                                 child: Icon(
-                                  Icons.menu,
+                                  iconAction[index]['icon'],
+                                  size: 20,
                                   color: white,
                                 ),
-                              ),
-                            ),
-                      const SizedBox(
-                        width: 7,
-                      ),
-                    ]),
-                TabBar(
-                    isScrollable: true,
-                    controller: _tabController,
-                    onTap: (index) {},
-                    indicatorColor: Colors.white,
-                    labelColor: Colors.white,
-                    unselectedLabelColor: Colors.white,
-                    indicatorSize: TabBarIndicatorSize.label,
-                    indicatorWeight: 1,
-                    tabs: const [
-                      Tab(
-                        text: "Đang theo dõi",
-                      ),
-                      Tab(
-                        text: "Dành cho bạn",
-                      )
-                    ]),
-                Row(
-                  children: List.generate(
-                      iconAction.length,
-                      (index) => Container(
-                            width: 30,
-                            height: 30,
-                            margin: const EdgeInsets.only(left: 5),
-                            decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Colors.grey.withOpacity(0.3)),
-                            child: Icon(
-                              iconAction[index]['icon'],
-                              size: 20,
-                              color: white,
-                            ),
-                          )),
-                )
-              ],
-            ),
-            backgroundColor: transparent, //No more green
-            elevation: 0.0, //Shadow gone
-          ))
-    ]));
+                              )),
+                    )
+                  ],
+                ),
+                backgroundColor: transparent, //No more green
+                elevation: 0.0, //Shadow gone
+              ))
+        ]));
   }
 
   @override
