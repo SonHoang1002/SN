97d75db9e7d93294e2c43d3bd223f251cd044767
tneils218 <trantrungnhat2512@gmail.com>
2023-05-11 16:19:53 +0700

    fix(*): learn_space render load image

--- lib/screen/LearnSpace/learn_space_host.dart
+++ lib/screen/LearnSpace/learn_space_host.dart
@@ -1,12 +1,13 @@
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:social_network_app_mobile/constant/common.dart';
 import 'package:social_network_app_mobile/helper/common.dart';
 import 'package:social_network_app_mobile/providers/learn_space/learn_space_provider.dart';
 import 'package:social_network_app_mobile/screen/LearnSpace/learn_space_detail.dart';
 import 'package:social_network_app_mobile/theme/colors.dart';
 import 'package:social_network_app_mobile/widget/card_components.dart';
-import 'package:social_network_app_mobile/widget/image_cache.dart';
 
 class LearnSpaceHost extends ConsumerStatefulWidget {
   const LearnSpaceHost({Key? key}) : super(key: key);
@@ -18,53 +19,51 @@ class LearnSpaceHost extends ConsumerStatefulWidget {
 class _LearnSpaceHostState extends ConsumerState<LearnSpaceHost> {
   late double width;
   late double height;
-  List course = [];
   var paramsConfigList = {"limit": 10, "only_current_user": true};
   final scrollController = ScrollController();
+  bool isLoading = false;
+
   @override
   void initState() {
     super.initState();
     if (mounted) {
-      Future.delayed(Duration.zero).then((_) {
-        ref
-            .read(learnSpaceStateControllerProvider.notifier)
-            .getListCoursesChipMenu(paramsConfigList)
-            .then((value) {
-          setState(() {
-            course =
-                ref.watch(learnSpaceStateControllerProvider).coursesChipMenu;
-          });
-        }).catchError((error) {
-          // handle error
-        });
+      Future.delayed(Duration.zero, () {
+        fetchData(paramsConfigList);
       });
     }
     scrollController.addListener(() {
       if (scrollController.position.maxScrollExtent ==
           scrollController.offset) {
-        String maxId = ref
-            .read(learnSpaceStateControllerProvider)
-            .coursesChipMenu
-            .last['id'];
-        ref
-            .read(learnSpaceStateControllerProvider.notifier)
-            .getListCoursesChipMenu({"max_id": maxId, ...paramsConfigList});
+        String maxId =
+            ref.read(learnSpaceStateControllerProvider).coursesHost.last['id'];
+        fetchData({"max_id": maxId, ...paramsConfigList});
       }
+    });
+  }
+
+  void fetchData(params) async {
+    setState(() {
+      isLoading = true;
+    });
+    await ref
+        .read(learnSpaceStateControllerProvider.notifier)
+        .getListCoursesHost(params);
+    setState(() {
+      isLoading = false;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     bool isMore = ref.watch(learnSpaceStateControllerProvider).isMore;
+    List course = ref.watch(learnSpaceStateControllerProvider).coursesHost;
     final size = MediaQuery.of(context).size;
     width = size.width;
     height = size.height;
     return Expanded(
         child: RefreshIndicator(
       onRefresh: () async {
-        ref
-            .read(learnSpaceStateControllerProvider.notifier)
-            .getListCoursesChipMenu(paramsConfigList);
+        fetchData(paramsConfigList);
       },
       child: SingleChildScrollView(
         controller: scrollController,
@@ -89,113 +88,115 @@ class _LearnSpaceHostState extends ConsumerState<LearnSpaceHost> {
                     itemCount: course.length,
                     scrollDirection: Axis.vertical,
                     itemBuilder: (context, index) {
-                      if (index < course.length) {
-                        return Padding(
-                          padding: const EdgeInsets.only(
-                              top: 8.0, left: 8.0, right: 8.0, bottom: 8.0),
-                          child: CardComponents(
-                            type: 'homeScreen',
-                            imageCard: ClipRRect(
-                              borderRadius: const BorderRadius.only(
-                                  topLeft: Radius.circular(15),
-                                  topRight: Radius.circular(15)),
-                              child: ImageCacheRender(
-                                path: course[index]['banner'] != null
-                                    ? course[index]['banner']['url']
-                                    : "https://sn.emso.vn/static/media/group_cover.81acfb42.png",
-                              ),
-                            ),
-                            onTap: () {
-                              Navigator.push(
-                                  context,
-                                  CupertinoPageRoute(
-                                      builder: (context) => LearnSpaceDetail(
-                                          data: course[index])));
-                            },
-                            textCard: Padding(
-                              padding: const EdgeInsets.only(
-                                  bottom: 16.0,
-                                  right: 16.0,
-                                  left: 16.0,
-                                  top: 8),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.all(2.0),
-                                    child: Text(
-                                      course[index]['title'],
-                                      maxLines: 2,
-                                      style: const TextStyle(
-                                        fontSize: 16.0,
-                                        fontWeight: FontWeight.w800,
-                                        overflow: TextOverflow.ellipsis,
-                                      ),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(2.0),
-                                    child: Text(
-                                      course[index]['account']['display_name'],
-                                      style: const TextStyle(
-                                        fontSize: 12.0,
-                                        color: greyColor,
-                                        fontWeight: FontWeight.w700,
-                                      ),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(2.0),
-                                    child: Text(
-                                      course[index]['status'] == 'draft'
-                                          ? 'Bản nháp'
-                                          : course[index]['status'] ==
-                                                  'approved'
-                                              ? 'Đã duyệt'
-                                              : 'Đang chờ',
-                                      style: TextStyle(
-                                        fontSize: 13.0,
-                                        color:
-                                            course[index]['status'] == 'draft'
-                                                ? Colors.green
-                                                : course[index]['status'] ==
-                                                        'approved'
-                                                    ? Colors.yellow
-                                                    : Colors.red,
-                                        fontWeight: FontWeight.w700,
-                                      ),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(2.0),
-                                    child: Text(
-                                      course[index]['free'] == true
-                                          ? 'Miễn phí'
-                                          : '${convertNumberToVND(course[index]['price'] ~/ 1)} VNĐ',
-                                      style: const TextStyle(
-                                        fontSize: 13.0,
-                                        color: secondaryColor,
-                                        fontWeight: FontWeight.w700,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
+                      return Padding(
+                        padding: const EdgeInsets.only(
+                            top: 8.0, left: 8.0, right: 8.0, bottom: 8.0),
+                        child: CardComponents(
+                          type: 'homeScreen',
+                          imageCard: ClipRRect(
+                            borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(15),
+                                topRight: Radius.circular(15)),
+                            child: ExtendedImage.network(
+                              course[index]['banner'] != null
+                                  ? course[index]['banner']['url']
+                                  : linkBannerDefault,
+                              fit: BoxFit.cover,
                             ),
                           ),
-                        );
-                      } else {
-                        isMore == true
-                            ? const Center(child: CupertinoActivityIndicator())
-                            : const SizedBox();
-                      }
-                      return null;
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                CupertinoPageRoute(
+                                    builder: (context) =>
+                                        LearnSpaceDetail(data: course[index])));
+                          },
+                          textCard: Padding(
+                            padding: const EdgeInsets.only(
+                                bottom: 16.0, right: 16.0, left: 16.0, top: 8),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(2.0),
+                                  child: Text(
+                                    course[index]['title'],
+                                    maxLines: 2,
+                                    style: const TextStyle(
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.w800,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(2.0),
+                                  child: Text(
+                                    course[index]['account']['display_name'],
+                                    style: const TextStyle(
+                                      fontSize: 12.0,
+                                      color: greyColor,
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(2.0),
+                                  child: Text(
+                                    course[index]['status'] == 'draft'
+                                        ? 'Bản nháp'
+                                        : course[index]['status'] == 'approved'
+                                            ? 'Đã duyệt'
+                                            : 'Đang chờ',
+                                    style: TextStyle(
+                                      fontSize: 13.0,
+                                      color: course[index]['status'] == 'draft'
+                                          ? Colors.green
+                                          : course[index]['status'] ==
+                                                  'approved'
+                                              ? Colors.yellow
+                                              : Colors.red,
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(2.0),
+                                  child: Text(
+                                    course[index]['free'] == true
+                                        ? 'Miễn phí'
+                                        : '${convertNumberToVND(course[index]['price'] ~/ 1)} VNĐ',
+                                    style: const TextStyle(
+                                      fontSize: 13.0,
+                                      color: secondaryColor,
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
                     },
                   ))
                 : const SizedBox(),
-            isMore == true
+            isLoading && course.isEmpty || isMore == true
                 ? const Center(child: CupertinoActivityIndicator())
-                : const SizedBox()
+                : course.isEmpty
+                    ? Column(
+                        children: [
+                          Center(
+                            child: Image.asset(
+                              "assets/wow-emo-2.gif",
+                              height: 125.0,
+                              width: 125.0,
+                            ),
+                          ),
+                          const Text('Không tìm thấy kết quả nào'),
+                        ],
+                      )
+                    : const SizedBox()
           ],
         ),
       ),
