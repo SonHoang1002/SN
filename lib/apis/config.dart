da5afcde144517163d2e3e2f7aa8983a4bf9ca13
luuhoangthanh <hoangthanhluu1998@gmail.com>
2023-05-20 16:28:39 +0700

    chore(config): config cmc

--- lib/apis/config.dart
+++ lib/apis/config.dart
@@ -1,26 +1,8 @@
-const baseRoot = 'https://snapi.emso.asia';
-const baseRootPtube = 'https://pt3.emso.vn';
-
-// const BASE_ROOT = 'https://mxh.emso.asia';
-const tokenVideoUpload = '21fbd3b2e427d9b9bae405e6480695e6bd233e44';
-// const userToken = '3cp6cSCoQqMK9HWCasDyyICcy52EfdVXVo_gx21I6ks';
-// const userToken = '2SeHv0_XX2DjiYhAoEqzdwSiSRSCCSOLhQgCLRP4eIA';
-//a Luu
-// const userToken = 'zRo-Aka_TJvXPzLJdfbHTs6SnLPEU8cHalDViC2Aixs';
-//a Dat
-const userToken = '1WJ_KeBJDF9pLtaUeV33sXjCshUzEDcanptxXD0Vn7A';
-
-const urlWebEmso = 'https://sn.emso.vn';
-
-const gifKey = "sXpGFDGZs0Dv1mmNFvYaGUvYwKX0PWIh";
-
-// const baseRoot = 'https://cmc-sn.emso.vn';
+// const baseRoot = 'https://snapi.emso.asia';
 // const baseRootPtube = 'https://pt3.emso.vn';
 
 // // const BASE_ROOT = 'https://mxh.emso.asia';
-
 // const tokenVideoUpload = '21fbd3b2e427d9b9bae405e6480695e6bd233e44';
-
 // // const userToken = '3cp6cSCoQqMK9HWCasDyyICcy52EfdVXVo_gx21I6ks';
 // // const userToken = '2SeHv0_XX2DjiYhAoEqzdwSiSRSCCSOLhQgCLRP4eIA';
 // //a Luu
@@ -28,6 +10,26 @@ const gifKey = "sXpGFDGZs0Dv1mmNFvYaGUvYwKX0PWIh";
 // //a Dat
 // const userToken = '1WJ_KeBJDF9pLtaUeV33sXjCshUzEDcanptxXD0Vn7A';
 
-// const urlWebEmso = 'https://cmc-fe.emso.vn';
+// const urlWebEmso = 'https://sn.emso.vn';
 
 // const gifKey = "sXpGFDGZs0Dv1mmNFvYaGUvYwKX0PWIh";
+
+//===============
+
+const baseRoot = 'https://cmc-sn.emso.vn';
+const baseRootPtube = 'https://pt3.emso.vn';
+
+// const BASE_ROOT = 'https://mxh.emso.asia';
+
+const tokenVideoUpload = '21fbd3b2e427d9b9bae405e6480695e6bd233e44';
+
+// const userToken = '3cp6cSCoQqMK9HWCasDyyICcy52EfdVXVo_gx21I6ks';
+// const userToken = '2SeHv0_XX2DjiYhAoEqzdwSiSRSCCSOLhQgCLRP4eIA';
+//a Luu
+// const userToken = 'zRo-Aka_TJvXPzLJdfbHTs6SnLPEU8cHalDViC2Aixs';
+//a Dat
+const userToken = '1WJ_KeBJDF9pLtaUeV33sXjCshUzEDcanptxXD0Vn7A';
+
+const urlWebEmso = 'https://cmc-fe.emso.vn';
+
+const gifKey = "sXpGFDGZs0Dv1mmNFvYaGUvYwKX0PWIh";
