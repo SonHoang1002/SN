10ed987c4cc74922bd20f39fcb3a4a1e4f474a9b
luuhoangthanh <hoangthanhluu1998@gmail.com>
2023-05-25 18:10:13 +0700

    handle drag watch

--- lib/screen/Watch/watch_detail.dart
+++ lib/screen/Watch/watch_detail.dart
@@ -3,13 +3,18 @@ import 'package:social_network_app_mobile/widget/FeedVideo/video_player_controll
 
 class WatchDetail extends StatefulWidget {
   final dynamic media;
-  const WatchDetail({Key? key, this.media}) : super(key: key);
+  final dynamic post;
+  const WatchDetail({Key? key, this.media, this.post}) : super(key: key);
 
   @override
   State<WatchDetail> createState() => _WatchDetailState();
 }
 
 class _WatchDetailState extends State<WatchDetail> {
+  double topPosition = 0;
+  double leftPosition = 0;
+  double scale = 1;
+
   @override
   Widget build(BuildContext context) {
     String averageColor = widget.media['meta']['original']['average_color'];
@@ -18,12 +23,19 @@ class _WatchDetailState extends State<WatchDetail> {
         body: Container(
       color: Color(int.parse('0xFF${averageColor.substring(1)}')),
       child: Center(
-        child: Hero(
-          tag: widget.media['remote_url'] ?? widget.media['url'],
-          child: VideoPlayerHasController(
-            media: widget.media,
-          ),
-        ),
+        child: Dismissible(
+            direction: DismissDirection.vertical,
+            key: const Key('page2'),
+            resizeDuration: const Duration(milliseconds: 1),
+            onDismissed: (direction) {
+              Navigator.pop(context);
+            },
+            child: Hero(
+              tag: widget.media['remote_url'] ?? widget.media['url'],
+              child: VideoPlayerHasController(
+                media: widget.media,
+              ),
+            )),
       ),
     ));
   }
