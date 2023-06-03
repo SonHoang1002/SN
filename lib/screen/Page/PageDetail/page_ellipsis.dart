e4a76c30a5690a81c278d077f512f15b21e2474b
tneils218 <trantrungnhat2512@gmail.com>
2023-06-03 17:31:06 +0700

    feat(*): emso icons, page ellipsis

--- lib/screen/Page/PageDetail/page_ellipsis.dart
+++ lib/screen/Page/PageDetail/page_ellipsis.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:social_network_app_mobile/widget/appbar_title.dart';
-
-import '../../../widget/EmsoIcons/emso_icons.dart';
 
 class PageEllipsis extends StatefulWidget {
   const PageEllipsis({super.key});
@@ -15,10 +12,28 @@ List pageEllipsis = [
   {
     "id": "report",
     "label": "Báo cáo trang cá nhân",
+    "icon": "assets/pages/profileReport.png",
   },
-  {},
-  {},
-  {},
+  {
+    "id": "block",
+    "label": "Chặn",
+    "icon": "assets/pages/block.png",
+  },
+  {
+    "id": "search",
+    "label": "Tìm kiếm",
+    "icon": "assets/pages/search.png",
+  },
+  {
+    "id": "invite",
+    "label": "Mời bạn bè",
+    "icon": "assets/pages/inviteFriend.png",
+  },
+  {
+    "id": "follow",
+    "label": "Theo dõi",
+    "icon": "assets/pages/inviteFriend.png",
+  },
 ];
 
 class _PageEllipsisState extends State<PageEllipsis> {
@@ -30,11 +45,32 @@ class _PageEllipsisState extends State<PageEllipsis> {
         centerTitle: true,
         title: const AppBarTitle(title: 'Quản lý'),
       ),
-      body: Row(
-        children: const [
-          Icon(EmsoIcons.squareExclamation),
-          Icon(FontAwesomeIcons.squareArrowUpRight),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ListView.builder(
+                padding: EdgeInsets.zero,
+                shrinkWrap: true,
+                itemCount: pageEllipsis.length,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      ListTile(
+                        horizontalTitleGap: 0,
+                        dense: true,
+                        leading: Image.asset(pageEllipsis[index]['icon'],
+                            width: 22, height: 22, color: Colors.white),
+                        title: Text(pageEllipsis[index]['label']),
+                      ),
+                      const Divider(height: 10, indent: 1, color: Colors.black),
+                    ],
+                  );
+                }),
+            const Text('Liên kết đến trang của ...'),
+            const Text('Liên kết riêng của ... trên EMSO'),
+          ],
+        ),
       ),
     );
   }
