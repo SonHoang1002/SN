2889484baa14d73f57ec788dae5e3cbeec4617ad
Vũ Tuấn Đạt <tuanvutruonglacnhatsinh0801@gmail.com>
2023-06-13 15:22:07 +0700

    feat: saved page (1)

---
+++ lib/screens/Saved/saved.dart
@@ -0,0 +1,200 @@
+import 'package:extended_image/extended_image.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:social_network_app_mobile/apis/bookmark_api.dart';
+import 'package:social_network_app_mobile/theme/colors.dart';
+import 'package:social_network_app_mobile/theme/theme_manager.dart';
+import 'package:provider/provider.dart' as pv;
+import 'package:social_network_app_mobile/widgets/card_components.dart';
+
+class Saved extends StatefulWidget {
+  const Saved({super.key});
+
+  @override
+  SavedState createState() => SavedState();
+}
+
+class SavedState extends State<Saved> {
+  List collections = [];
+  List bookmarks = [];
+
+  void initBookmark() async {
+    // get all bookmark of users
+    var bmResult = await BookmarkApi().fetchAllBookmark();
+
+    // get all collection of users
+    var cltResult = await BookmarkApi().fetchBookmarkCollection();
+
+    var renderCollections = [];
+    for (var i = 0; i < cltResult.length; i++) {
+      var collection = cltResult[i];
+      var bookmarks = bmResult
+          .where((e) =>
+              e['bookmark_collection'] != null &&
+              e['bookmark_collection']['id'] == collection['id'])
+          .toList();
+
+      if (bookmarks.isNotEmpty) {
+        var earliest =
+            DateTime.parse(bookmarks[0]['created_at']).millisecondsSinceEpoch;
+        var index = 0;
+        for (var j = 0; j < bookmarks.length; j++) {
+          int createAt =
+              DateTime.parse(bookmarks[j]['created_at']).millisecondsSinceEpoch;
+          if (createAt <= earliest) {
+            earliest = createAt;
+            index = j;
+          }
+        }
+        if (bmResult[index]['status']['card'] != null) {
+          collection['imageUrl'] = bmResult[index]['status']['card']['image'];
+        } else if (bmResult[index]['status']['account']['avatar_media']
+                ['url'] !=
+            null) {
+          collection['imageUrl'] =
+              bmResult[index]['status']['account']['avatar_media']['url'];
+        } else {
+          collection['imageUrl'] =
+              bmResult[index]['status']['account']['avatar_static'];
+        }
+      }
+      renderCollections.add(collection);
+    }
+
+    setState(() {
+      collections = renderCollections;
+      bookmarks = bmResult;
+    });
+  }
+
+  @override
+  void initState() {
+    initBookmark();
+    super.initState();
+  }
+
+  Widget _buildCollections(
+    dynamic collections,
+    double height,
+    ThemeManager theme,
+  ) {
+    return Container(
+      child: GridView.builder(
+        clipBehavior: Clip.hardEdge,
+        shrinkWrap: true,
+        itemCount: collections.length >= 4 ? 4 : collections.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisSpacing: 5.0,
+          mainAxisSpacing: 5.0,
+          crossAxisCount: 2,
+        ),
+        itemBuilder: (context, index) {
+          var item = collections[index];
+          return Container(
+            child: CardComponents(
+              imageCard: SizedBox(
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
+                  child: Image.network(
+                    item['imageUrl'],
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                    height: height / 8,
+                  ),
+                ),
+              ),
+              onTap: () {},
+              textCard: Container(
+                // color: Colors.red,
+                padding: const EdgeInsets.only(
+                  right: 12.5,
+                  left: 12.5,
+                  top: 8,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: Text(
+                        item['name'],
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w800,
+                          color: theme.isDarkMode ? Colors.white : Colors.black,
+                        ),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.all(2.0),
+                      child: Text(
+                        'Chỉ mình tôi',
+                        style: TextStyle(
+                          fontSize: 12.5,
+                          color: greyColor,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildRowAction(ThemeManager theme) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          "Bộ sưu tập",
+          style: TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.bold,
+            color: theme.isDarkMode ? Colors.white : Colors.black,
+          ),
+        ),
+        TextButton(
+          onPressed: () {},
+          child: const Text(
+            "Tạo",
+            style: TextStyle(
+              fontSize: 16.0,
+              color: Colors.lightBlue,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    final theme = pv.Provider.of<ThemeManager>(context);
+
+    return Container(
+      width: width,
+      height: height,
+      padding: const EdgeInsets.symmetric(horizontal: 12.5, vertical: 8.0),
+      child: SingleChildScrollView(
+        child: collections.isEmpty || bookmarks.isEmpty
+            ? const Center(child: CupertinoActivityIndicator())
+            : Column(
+                children: [
+                  _buildRowAction(theme),
+                  _buildCollections(collections, height, theme),
+                ],
+              ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/modal_bookmark.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:social_network_app_mobile/apis/bookmark_api.dart';
+import 'package:social_network_app_mobile/model/bookmark.dart';
+import 'package:social_network_app_mobile/theme/theme_manager.dart';
+import 'package:social_network_app_mobile/widgets/appbar_title.dart';
+import 'package:provider/provider.dart' as pv;
+
+class ModalBookmark extends StatefulWidget {
+  const ModalBookmark({super.key});
+
+  @override
+  ModalBookmarkState createState() => ModalBookmarkState();
+}
+
+class ModalBookmarkState extends State<ModalBookmark> {
+  List<dynamic> collections = [];
+  String _bookmarkCollectionId = "";
+  var _controller = TextEditingController();
+
+  void getBookmarkCollection() async {
+    var result = await BookmarkApi().fetchBookmarkCollection();
+    setState(() {
+      collections = result;
+      _bookmarkCollectionId = result[0]['id'];
+    });
+  }
+
+  @override
+  void initState() {
+    getBookmarkCollection();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = pv.Provider.of<ThemeManager>(context);
+    final titleStyle = TextStyle(
+      fontSize: 17.5,
+      color: theme.isDarkMode ? Colors.white : Colors.black,
+      fontWeight: FontWeight.bold,
+    );
+    return SingleChildScrollView(
+      child: Container(
+        height: MediaQuery.of(context).size.height * 0.55,
+        padding: const EdgeInsets.symmetric(vertical: 25.0),
+        child: Column(
+          children: [
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).pop();
+                showDialog<void>(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text('Tạo bộ sưu tập', style: titleStyle),
+                      content: TextFormField(
+                        style: const TextStyle(fontSize: 18.0),
+                        cursorColor: Colors.blue,
+                        controller: _controller,
+                        autofocus: true,
+                        decoration: const InputDecoration(
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.blue),
+                          ),
+                        ),
+                      ),
+                      actions: <Widget>[
+                        TextButton(
+                          style: TextButton.styleFrom(
+                            textStyle: Theme.of(context).textTheme.labelLarge,
+                          ),
+                          child: Text('Hủy', style: titleStyle),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                        TextButton(
+                          style: TextButton.styleFrom(
+                            textStyle: Theme.of(context).textTheme.labelLarge,
+                          ),
+                          child: const Text('Tạo',
+                              style: TextStyle(
+                                fontSize: 17.5,
+                                color: Colors.blue,
+                                fontWeight: FontWeight.bold,
+                              )),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: CircleAvatar(
+                      radius: 20.0,
+                      backgroundColor: Colors.grey[350],
+                      child: const Icon(
+                        FontAwesomeIcons.plus,
+                        size: 20.0,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 4,
+                    child: Text("Bộ sưu tập mới", style: titleStyle),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 12.5),
+            ListView.builder(
+              itemCount: collections.length,
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                final collection = collections[index];
+                return Container(
+                  margin: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: CircleAvatar(
+                          radius: 20.0,
+                          backgroundColor: Colors.lightBlue[200],
+                          child: Icon(
+                            FontAwesomeIcons.bookmark,
+                            size: 20.0,
+                            color:
+                                theme.isDarkMode ? Colors.white : Colors.black,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 3,
+                        child: Text(collection['name'], style: titleStyle),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Radio<String>(
+                          value: collection['id'],
+                          groupValue: _bookmarkCollectionId,
+                          onChanged: (String? value) {
+                            setState(() {
+                              _bookmarkCollectionId = value!;
+                            });
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
