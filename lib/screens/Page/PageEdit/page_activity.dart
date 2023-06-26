366e480adfcbb10db2a5e35001604389d53e5e8e
tneils218 <trantrungnhat2512@gmail.com>
2023-06-26 10:48:17 +0700

    feat(*): text bold display_name

--- lib/screens/Page/PageEdit/page_activity.dart
+++ lib/screens/Page/PageEdit/page_activity.dart
@@ -1,5 +1,6 @@
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
+import 'package:get_time_ago/get_time_ago.dart';
 import 'package:social_network_app_mobile/apis/page_api.dart';
 import 'package:social_network_app_mobile/widgets/appbar_title.dart';
 
@@ -29,21 +30,55 @@ class _PageActivityState extends State<PageActivity> {
     }
   }
 
-  String extractDescription(dynamic object) {
+  Widget extractDescription(dynamic object) {
     String description = object["description"].toString();
     description = description.replaceAll(RegExp(r'\s{2,}'), ' ');
+
     if (object["includes"] != null && object["includes"].isNotEmpty) {
       for (var include in object["includes"]) {
         if (include["id"] != null && include["title"] != null) {
           final id = include["id"].toString();
           final title = include["title"].toString();
+
           if (description.contains('[$id]')) {
-            description = description.replaceAll('[$id]', title);
+            description = description.replaceAll('[$id]', '[$title]');
           }
         }
       }
     }
-    return description;
+
+    List<TextSpan> textSpans = [];
+    List<String> words = description.split(' ');
+
+    for (var word in words) {
+      if (word.startsWith('[') && word.endsWith(']')) {
+        String title = word.substring(1, word.length - 1);
+        textSpans.add(
+          TextSpan(
+            text: title,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        );
+      } else {
+        textSpans.add(
+          TextSpan(
+            text: word,
+            style: const TextStyle(
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+        );
+      }
+      textSpans.add(const TextSpan(text: ' '));
+    }
+
+    return RichText(
+      text: TextSpan(
+        children: textSpans,
+      ),
+    );
   }
 
   String extractImage(dynamic object) {
@@ -74,16 +109,18 @@ class _PageActivityState extends State<PageActivity> {
           itemCount: data.length,
           itemBuilder: (context, index) {
             return ListTile(
-              contentPadding:
-                  const EdgeInsets.symmetric(horizontal: 8.0, vertical: 0.0),
-              visualDensity: const VisualDensity(horizontal: -4, vertical: 0),
-              leading: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ClipOval(
-                    child: ExtendedImage.network(extractImage(data[index]))),
-              ),
-              title: Text(extractDescription(data[index])),
-            );
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 0.0),
+                visualDensity: const VisualDensity(horizontal: -4, vertical: 0),
+                leading: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ClipOval(
+                      child: ExtendedImage.network(extractImage(data[index]))),
+                ),
+                title: extractDescription(data[index]),
+                subtitle: Text(GetTimeAgo.parse(
+                    DateTime.parse(data[index]['created_at']))),
+                onTap: () {});
           }),
     );
   }
