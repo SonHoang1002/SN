02b3b80c93143758d6902c49821a69530e0afb58
tneils218 <trantrungnhat2512@gmail.com>
2023-06-29 09:26:32 +0700

    feat(*): menu manager

--- lib/screens/Group/GroupDetail/home_group.dart
+++ lib/screens/Group/GroupDetail/home_group.dart
@@ -5,6 +5,7 @@ import 'package:social_network_app_mobile/constant/common.dart';
 import 'package:social_network_app_mobile/screens/Group/GroupDetail/manager.dart';
 import 'package:social_network_app_mobile/widgets/AvatarStack/avatar_stack.dart';
 import 'package:social_network_app_mobile/widgets/button_primary.dart';
+import 'package:social_network_app_mobile/widgets/chip_menu.dart';
 
 import '../../../widgets/AvatarStack/positions.dart';
 
@@ -20,6 +21,8 @@ class HomeGroup extends StatefulWidget {
 }
 
 class _HomeGroupState extends State<HomeGroup> {
+  String menuSelected = '';
+
   final settings = RestrictedPositions(
     maxCoverage: 0.3,
     minCoverage: 0.2,
@@ -30,162 +33,208 @@ class _HomeGroupState extends State<HomeGroup> {
     return url;
   }
 
+  List groupChip = [
+    {
+      'key': 'intro',
+      'label': 'Giới thiệu',
+    },
+    {
+      'key': 'interestion',
+      'label': 'Đáng chú ý',
+    },
+    {
+      'key': 'image',
+      'label': 'Ảnh',
+    },
+    {
+      'key': 'event',
+      'label': 'Sự kiện',
+    },
+    {
+      'key': 'album',
+      'label': 'Album',
+    },
+  ];
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Stack(
-              children: [
-                SizedBox(
-                  width: size.width,
-                  height: size.height * 0.25,
-                  child: ExtendedImage.network(
-                    widget.groupDetail['banner'] != null
-                        ? widget.groupDetail['banner']['preview_url']
-                        : linkBannerDefault,
-                    fit: BoxFit.cover,
+      scrollDirection: Axis.vertical,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Stack(
+            children: [
+              SizedBox(
+                width: size.width,
+                height: size.height * 0.25,
+                child: ExtendedImage.network(
+                  widget.groupDetail['banner'] != null
+                      ? widget.groupDetail['banner']['preview_url']
+                      : linkBannerDefault,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Positioned(
+                right: 10,
+                bottom: 0,
+                child: ElevatedButton.icon(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(0, 30),
+                    elevation: 0,
+                    backgroundColor: Colors.transparent.withOpacity(0.5),
+                    shadowColor: Colors.transparent.withOpacity(0.5),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4)),
+                  ),
+                  icon: const Icon(
+                    Icons.edit,
+                    color: Colors.white,
+                    size: 18,
+                  ),
+                  label: const Text(
+                    'Chỉnh sửa',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
                   ),
                 ),
-                Positioned(
-                  right: 10,
-                  bottom: 0,
-                  child: ElevatedButton.icon(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size(0, 30),
-                      elevation: 0,
-                      backgroundColor: Colors.transparent.withOpacity(0.5),
-                      shadowColor: Colors.transparent.withOpacity(0.5),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(4)),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                RichText(
+                    text: TextSpan(
+                  children: [
+                    TextSpan(
+                        text: widget.groupDetail['title'],
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        )),
+                    const WidgetSpan(
+                        child: Padding(
+                      padding: EdgeInsets.only(left: 5.0),
+                      child: Icon(Icons.chevron_right,
+                          size: 18, color: Colors.grey),
+                    )),
+                  ],
+                )),
+                const SizedBox(
+                  height: 10,
+                ),
+                RichText(
+                    text: TextSpan(
+                  children: [
+                    const WidgetSpan(
+                        child: Padding(
+                      padding: EdgeInsets.only(bottom: 1.0, right: 5.0),
+                      child: Icon(Icons.lock, size: 14, color: Colors.grey),
+                    )),
+                    TextSpan(
+                        text: widget.groupDetail['is_private'] == true
+                            ? 'Nhóm Riêng tư'
+                            : '',
+                        style: const TextStyle(
+                          fontSize: 14,
+                          color: Colors.white,
+                        )),
+                    TextSpan(
+                        text:
+                            ' \u{2022} ${widget.groupDetail['member_count']} thành viên',
+                        style: const TextStyle(
+                          fontSize: 14,
+                          color: Colors.white,
+                        )),
+                  ],
+                )),
+                const SizedBox(
+                  height: 10,
+                ),
+                AvatarStack(
+                  height: 40,
+                  borderColor: Theme.of(context).scaffoldBackgroundColor,
+                  settings: settings,
+                  iconEllipse: true,
+                  avatars: [
+                    for (var n = 0; n < 7; n++)
+                      NetworkImage(
+                        getAvatarUrl(n),
+                      ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: size.width * 0.44,
+                      child: ButtonPrimary(
+                        label: 'Quản lý',
+                        icon: Image.asset('assets/groups/managerGroup.png',
+                            width: 16, height: 16),
+                        handlePress: () {
+                          Navigator.push(
+                              context,
+                              CupertinoPageRoute(
+                                  builder: (context) => ManagerDetail(
+                                      groupDetail: widget.groupDetail)));
+                        },
+                      ),
                     ),
-                    icon: const Icon(
-                      Icons.edit,
-                      color: Colors.white,
-                      size: 18,
+                    const SizedBox(
+                      width: 8,
                     ),
-                    label: const Text(
-                      'Chỉnh sửa',
-                      style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.bold),
+                    SizedBox(
+                      width: size.width * 0.44,
+                      child: ButtonPrimary(
+                        label: 'Mời',
+                        icon: Padding(
+                          padding: const EdgeInsets.only(bottom: 3.0),
+                          child: Image.asset('assets/groups/group.png',
+                              width: 16, height: 16),
+                        ),
+                        isGrey: true,
+                        handlePress: () {},
+                      ),
                     ),
-                  ),
+                  ],
                 ),
               ],
             ),
-            Padding(
-              padding:
-                  const EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  RichText(
-                      text: TextSpan(
-                    children: [
-                      TextSpan(
-                          text: widget.groupDetail['title'],
-                          style: const TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          )),
-                      const WidgetSpan(
-                          child: Padding(
-                        padding: EdgeInsets.only(left: 5.0),
-                        child: Icon(Icons.chevron_right,
-                            size: 18, color: Colors.grey),
-                      )),
-                    ],
-                  )),
-                  const SizedBox(
-                    height: 10,
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 10.0, right: 10.0, top: 8.0),
+              child: Row(
+                children: List.generate(
+                  groupChip.length,
+                  (index) => InkWell(
+                    onTap: () {
+                      setState(
+                        () {
+                          menuSelected = groupChip[index]['key'];
+                        },
+                      );
+                    },
+                    child: ChipMenu(
+                      isSelected: menuSelected == groupChip[index]['key'],
+                      label: groupChip[index]['label'],
+                    ),
                   ),
-                  RichText(
-                      text: TextSpan(
-                    children: [
-                      const WidgetSpan(
-                          child: Padding(
-                        padding: EdgeInsets.only(bottom: 1.0, right: 5.0),
-                        child: Icon(Icons.lock, size: 14, color: Colors.grey),
-                      )),
-                      TextSpan(
-                          text: widget.groupDetail['is_private'] == true
-                              ? 'Nhóm Riêng tư'
-                              : '',
-                          style: const TextStyle(
-                            fontSize: 14,
-                            color: Colors.white,
-                          )),
-                      TextSpan(
-                          text:
-                              ' \u{2022} ${widget.groupDetail['member_count']} thành viên',
-                          style: const TextStyle(
-                            fontSize: 14,
-                            color: Colors.white,
-                          )),
-                    ],
-                  )),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  AvatarStack(
-                    height: 40,
-                    borderColor: Theme.of(context).scaffoldBackgroundColor,
-                    settings: settings,
-                    iconEllipse: true,
-                    avatars: [
-                      for (var n = 0; n < 7; n++)
-                        NetworkImage(
-                          getAvatarUrl(n),
-                        ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    children: [
-                      SizedBox(
-                        width: size.width * 0.44,
-                        child: ButtonPrimary(
-                          label: 'Quản lý',
-                          icon: Image.asset('assets/groups/managerGroup.png',
-                              width: 16, height: 16),
-                          handlePress: () {
-                            Navigator.push(
-                                context,
-                                CupertinoPageRoute(
-                                    builder: (context) => ManagerDetail(
-                                        groupDetail: widget.groupDetail)));
-                          },
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 8,
-                      ),
-                      SizedBox(
-                        width: size.width * 0.44,
-                        child: ButtonPrimary(
-                          label: 'Mời',
-                          icon: Padding(
-                            padding: const EdgeInsets.only(bottom: 3.0),
-                            child: Image.asset('assets/groups/group.png',
-                                width: 16, height: 16),
-                          ),
-                          isGrey: true,
-                          handlePress: () {},
-                        ),
-                      ),
-                    ],
-                  )
-                ],
+                ),
               ),
-            )
-          ],
-        ));
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/screens/Group/GroupDetail/manager.dart
+++ lib/screens/Group/GroupDetail/manager.dart
@@ -9,17 +9,99 @@ class ManagerDetail extends StatefulWidget {
   State<ManagerDetail> createState() => _ManagerDetailState();
 }
 
+List requestManager = [
+  {
+    'key': 'content',
+    'label': 'Nội dung bị báo cáo',
+    'icon': 'assets/groups/contentReport.png',
+  },
+  {
+    'key': 'waiting',
+    'label': 'Đang chờ phê duyệt',
+    'icon': 'assets/groups/waitingRequest.png',
+  },
+  {
+    'key': 'member',
+    'label': 'Yêu cầu làm thành viên',
+    'icon': 'assets/groups/requestMember.png',
+  },
+  {
+    'key': 'noti',
+    'label': 'Thông báo kiểm duyệt',
+    'icon': 'assets/groups/notiRequest.png',
+  },
+  {
+    'key': 'spam',
+    'label': 'Có thể là spam',
+    'icon': 'assets/groups/canSpam.png',
+  },
+  {
+    'key': 'armorial',
+    'label': 'Yêu cầu huy hiệu',
+    'icon': 'assets/groups/requestArmorial.png',
+  },
+];
+
 class _ManagerDetailState extends State<ManagerDetail> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          centerTitle: true,
-          title: AppBarTitle(
-            title: widget.groupDetail['title'] ?? '',
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        title: AppBarTitle(
+          title: widget.groupDetail['title'] ?? '',
+        ),
+      ),
+      body: Container(
+        width: double.infinity,
+        color: Colors.grey[800],
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Cần xét duyệt',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(8)),
+                child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: requestManager.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        dense: true,
+                        contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 8.0, vertical: 0.0),
+                        visualDensity:
+                            const VisualDensity(horizontal: -4, vertical: -1),
+                        leading: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Image.asset(
+                            requestManager[index]['icon'],
+                            width: 18,
+                            height: 18,
+                          ),
+                        ),
+                        title: Text(requestManager[index]['label']),
+                      );
+                    }),
+              )
+            ],
           ),
         ),
-        body: Container());
+      ),
+    );
   }
 }
