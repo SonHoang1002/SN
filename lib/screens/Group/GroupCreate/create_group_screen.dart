86cdcfb1cde49ca4d25098fd0db95a3475cc0d05
tneils218 <trantrungnhat2512@gmail.com>
2023-07-10 10:39:07 +0700

    feat(*): create group screen

---
+++ lib/screens/Group/GroupCreate/create_group_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateGroup extends StatefulWidget {
+  const CreateGroup({super.key});
+
+  @override
+  State<CreateGroup> createState() => _CreateGroupState();
+}
+
+class _CreateGroupState extends State<CreateGroup> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
