2fbaefd58ee7ef2b6289537e5e92cf1e58dc9074
Haizzzzzzzzzzz <caiginhi103@gmail.com>
2023-08-03 17:54:48 +0700

    feature/suggest_friend-create_CV

--- lib/screens/Recruit/recruit_cv.dart
+++ lib/screens/Recruit/recruit_cv.dart
@@ -1,17 +1,20 @@
 import 'dart:async';
-
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:social_network_app_mobile/apis/config.dart';
 import 'package:social_network_app_mobile/constant/common.dart';
 import 'package:social_network_app_mobile/providers/recruit/recruit_provider.dart';
 import 'package:social_network_app_mobile/widgets/card_components.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+import '../../theme/colors.dart';
+
 class RecruitCV extends ConsumerStatefulWidget {
-  const RecruitCV({super.key});
+  final dynamic data;
+  const RecruitCV({super.key, this.data});
 
   @override
   ConsumerState<RecruitCV> createState() => _RecruitCVState();
@@ -32,14 +35,50 @@ class _RecruitCVState extends ConsumerState<RecruitCV> {
   Widget build(BuildContext context) {
     List recruitsCV = ref.watch(recruitControllerProvider).recruitsCV;
     bool isMore = ref.watch(recruitControllerProvider).isMore;
-
     final Completer<WebViewController> controller =
         Completer<WebViewController>();
     final size = MediaQuery.sizeOf(context);
     width = size.width;
     height = size.height;
-    return Expanded(
-        child: RefreshIndicator(
+    return Scaffold(
+        extendBodyBehindAppBar: true,
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title:const Center(
+          child:  Text(
+            'CV của bạn',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+
+            ),
+          ),
+        ),
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        leading: GestureDetector(
+            behavior: HitTestBehavior.opaque,
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Container(
+              height: 26,
+              width: 26,
+              margin: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.4),
+                  borderRadius: BorderRadius.circular(24),
+                  border: Border.all(width: 0.2, color: greyColor)),
+              child: const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(FontAwesomeIcons.angleLeft,
+                      color: Colors.white, size: 16),
+                ],
+              ),
+            )),
+        elevation: 0.0,
+      ),
+      body: RefreshIndicator(
       onRefresh: () async {
         Future.delayed(
             Duration.zero,
@@ -51,16 +90,6 @@ class _RecruitCVState extends ConsumerState<RecruitCV> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Padding(
-              padding: EdgeInsets.only(left: 16.0, right: 16.0, bottom: 8.0),
-              child: Text(
-                'Thư viện nội dung của bạn',
-                style: TextStyle(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
             recruitsCV.isNotEmpty
                 ? SizedBox(
                     child: ListView.builder(
@@ -88,7 +117,7 @@ class _RecruitCVState extends ConsumerState<RecruitCV> {
                             ),
                           ),
                           onTap: () {
-                            var id = recruitsCV[index]['id'];
+                            var id = widget.data['id'];
                             Navigator.push(
                               context,
                               MaterialPageRoute(
--- lib/widgets/Suggestions/suggest_item.dart
+++ lib/widgets/Suggestions/suggest_item.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -7,10 +9,13 @@ import 'package:social_network_app_mobile/constant/post_type.dart';
 import 'package:social_network_app_mobile/helper/common.dart';
 import 'package:social_network_app_mobile/providers/friend/friend_provider.dart';
 import 'package:social_network_app_mobile/providers/group/group_list_provider.dart';
-import 'package:social_network_app_mobile/theme/colors.dart';
 import 'package:social_network_app_mobile/widgets/GeneralWidget/spacer_widget.dart';
 import 'package:social_network_app_mobile/widgets/GeneralWidget/text_content_widget.dart';
 import 'package:social_network_app_mobile/widgets/button_primary.dart';
+
+import '../../apis/friends_api.dart';
+import '../../apis/group_api.dart';
+import '../../theme/colors.dart';
 
 class SuggestItem extends ConsumerWidget {
   final dynamic suggestData;
@@ -21,102 +26,136 @@ class SuggestItem extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    String idJoin = "";
     final size = MediaQuery.sizeOf(context);
-    return InkWell(
-      onTap: () {
-        // pushCustomCupertinoPageRoute(context, const Moment());
-      },
-      child: Container(
-          width: size.width * 0.8,
-          decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
-          child: Column(
-            children: [
-              Flexible(
-                  child: ExtendedImage.network(
-                suggestData["avatar_media"]?["url"] ??
-                    suggestData["banner"]?["preview_url"] ??
-                    linkBannerDefault,
-                height: size.width * 0.75,
-                fit: BoxFit.cover,
-                width: size.width * 0.75,
-              )),
-              buildSpacer(height: 10),
-              buildTextContent(
-                  suggestData["display_name"] ?? suggestData["title"] ?? "",
-                  true,
-                  fontSize: 15,
-                  margin: const EdgeInsets.only(left: 10)),
-              buildSpacer(height: 10),
-              type == suggestFriends
-                  ? buildTextContent(
-                      suggestData["relationships"]?["mutual_friend_count"] !=
-                              null
-                          ? "${suggestData["relationships"]?["mutual_friend_count"] ?? 0} bạn chung"
-                          : "",
-                      false,
-                      fontSize: 15,
-                      margin: const EdgeInsets.only(left: 10))
-                  : const SizedBox(),
-              type == suggestGroups
-                  ? buildTextContent(
-                      "${suggestData['member_count'] ?? 0} thành viên - ${suggestData['average_status'] ?? 0} bài viết/ngày",
-                      false,
-                      fontSize: 15,
-                      margin: const EdgeInsets.only(left: 10))
-                  : const SizedBox(),
-              buildTextContent(
-                  type == suggestGroups &&
-                          handleTimeShow(suggestData['last_status_at'])
-                              is String
-                      ? "Hoạt động gần nhất: ${handleTimeShow(suggestData['last_status_at'])!}"
-                      : "",
-                  false,
-                  fontSize: 15,
-                  margin: const EdgeInsets.only(left: 10)),
-              buildSpacer(height: 10),
-              Flex(
-                direction: Axis.horizontal,
-                children: [
-                  buildSpacer(width: 10),
-                  Flexible(
-                      flex: 8,
-                      child: ButtonPrimary(
-                        label: type == suggestFriends
-                            ? "Thêm bạn bè"
-                            : type == suggestGroups
-                                ? "Tham gia"
-                                : "",
-                        icon: const Icon(
-                          FontAwesomeIcons.user,
-                          size: 14,
-                        ),
-                        handlePress: () {},
-                      )),
-                  buildSpacer(width: 5),
-                  Flexible(
-                      flex: 3,
-                      child: ButtonPrimary(
-                        label: "Gỡ",
-                        colorText: Theme.of(context).textTheme.bodyLarge!.color,
-                        handlePress: () {
-                          if (type == suggestFriends) {
-                            ref
-                                .read(friendControllerProvider.notifier)
-                                .removeFriendSuggest(suggestData['id']);
-                          } else if (type == suggestGroups) {
-                            ref
-                                .read(groupListControllerProvider.notifier)
-                                .removeGroupMember(suggestData['id']);
-                          }
-                          reloadFunction != null ? reloadFunction!() : null;
-                        },
-                        colorButton: Theme.of(context).canvasColor,
-                      )),
-                  buildSpacer(width: 10),
-                ],
-              )
-            ],
-          )),
-    );
+    return StatefulBuilder(builder: (context, setState) {
+      return InkWell(
+        onTap: () {
+          // pushCustomCupertinoPageRoute(context, const Moment());
+        },
+        child: Container(
+            width: size.width * 0.8,
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+            child: Column(
+              children: [
+                Flexible(
+                    child: ExtendedImage.network(
+                  suggestData["avatar_media"]?["url"] ??
+                      suggestData["banner"]?["preview_url"] ??
+                      linkBannerDefault,
+                  height: size.width * 0.75,
+                  fit: BoxFit.cover,
+                  width: size.width * 0.75,
+                )),
+                buildSpacer(height: 10),
+                buildTextContent(
+                    suggestData["display_name"] ?? suggestData["title"] ?? "",
+                    true,
+                    fontSize: 15,
+                    margin: const EdgeInsets.only(left: 10)),
+                buildSpacer(height: 10),
+                type == suggestFriends
+                    ? buildTextContent(
+                        suggestData["relationships"]?["mutual_friend_count"] !=
+                                null
+                            ? "${suggestData["relationships"]?["mutual_friend_count"] ?? 0} bạn chung"
+                            : "",
+                        false,
+                        fontSize: 15,
+                        margin: const EdgeInsets.only(left: 10))
+                    : const SizedBox(),
+                type == suggestGroups
+                    ? buildTextContent(
+                        "${suggestData['member_count'] ?? 0} thành viên - ${suggestData['average_status'] ?? 0} bài viết/ngày",
+                        false,
+                        fontSize: 15,
+                        margin: const EdgeInsets.only(left: 10))
+                    : const SizedBox(),
+                buildTextContent(
+                    type == suggestGroups &&
+                            handleTimeShow(suggestData['last_status_at'])
+                                is String
+                        ? "Hoạt động gần nhất: ${handleTimeShow(suggestData['last_status_at'])!}"
+                        : "",
+                    false,
+                    fontSize: 15,
+                    margin: const EdgeInsets.only(left: 10)),
+                buildSpacer(height: 10),
+                Flex(
+                  direction: Axis.horizontal,
+                  children: [
+                    buildSpacer(width: idJoin == suggestData['id'] ? 3.5 : 10),
+                    Flexible(
+                        flex: 10,
+                        child: ButtonPrimary(
+                          colorButton: idJoin == suggestData['id']
+                                ? greyColor
+                                : null,
+                            label: type == suggestFriends
+                                ? idJoin == suggestData['id']
+                                    ? "Đã gửi lời mời kết bạn"
+                                    : "Thêm bạn bè"
+                                : type == suggestGroups? 
+                                idJoin == suggestData['id']
+                                    ? "Đã tham gia nhóm"
+                                    : "Tham gia nhóm":"",
+                            icon: idJoin != suggestData['id']? const Icon( 
+                              FontAwesomeIcons.user,
+                              size: 12.5,
+                              color: Colors.white,
+                            ):null,
+                            handlePress: () async {
+                              if (type == suggestFriends) {
+                                setState(() {
+                                  idJoin = suggestData['id'];
+                                });
+                                await FriendsApi()
+                                    .sendFriendRequestApi(suggestData['id']);
+                                await ref
+                                    .read(friendControllerProvider.notifier)
+                                    .removeFriendSuggest(suggestData['id']);
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(SnackBar(
+                                  content: Text(
+                                      'Đã gửi lời mời kết bạn tới ${suggestData["display_name"]}'),
+                                  duration: const Duration(seconds: 2),
+                                ));
+                              } else {
+                                await GroupApi()
+                                    .fetchGroupRole(suggestData['id']);
+                                
+                                setState(() {
+                                  idJoin = suggestData['id'];
+                                });
+                              }
+                            })),
+                    buildSpacer(width: 5),
+                    Flexible(
+                        flex: 3,
+                        child: ButtonPrimary(
+                            label: "Gỡ",
+                            colorText: Colors.black,
+                            handlePress: () {
+                              if (type == suggestFriends) {
+                                ref
+                                    .read(friendControllerProvider.notifier)
+                                    .removeFriendSuggest(suggestData['id']);
+                              } else if (type == suggestGroups) {
+                                ref
+                                    .read(groupListControllerProvider.notifier)
+                                    .removeGroupMember(suggestData['id']);
+                              }
+                              reloadFunction != null ? reloadFunction!() : null;
+                            },
+                            colorButton: ThemeMode.dark == true
+                                ? greyColor
+                                : const Color(0xfff1f2f5))),
+                    buildSpacer(width: 10),
+                  ],
+                )
+              ],
+            )),
+      );
+    });
   }
 }
