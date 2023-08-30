23e18112d93acdb65f5bd4153f3201dcb79cb451
thanh binh <binhthanhk2@gmail.com>
2023-08-30 17:48:36 +0700

    fix lỗi import

--- lib/screens/Group/GroupSetting/group_setting.dart
+++ lib/screens/Group/GroupSetting/group_setting.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:social_network_app_mobile/screens/UserPage/SettingUser/user_tag_settings.dart';
+import 'package:social_network_app_mobile/screens/UserPage/SettingUser/tag/user_tag_settings.dart';
+
 import 'package:social_network_app_mobile/theme/colors.dart';
 import 'package:social_network_app_mobile/theme/theme_manager.dart';
 import 'package:social_network_app_mobile/widgets/button_primary.dart';
